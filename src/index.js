44ccf1b37bed3acbd03b6a492fca9e6ccb53971a
Carlos Thompson <Carlos.Thompson@criticalsoftware.com>
2023-06-12 10:20:59 +0100

    Load products from list.json  with basic XMLHttpRequest

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 // main JS file that initiates this App/page JS modules
+
+let http = new XMLHttpRequest();
+
+http.open('get', './mocks/list.json', true);
+http.send();
+
+http.onload = function () {
+  if (this.readyState == 4  && this.status == 200 ) {
+    let products = JSON.parse(this.responseText);
+
+    let output = "";
+
+    for(let item of products) {
+      output += `
+        <article class="product">
+          <h2 class="product-header">${item.name}</h2>
+          <img class="product-image" src="${item.imagePath}" alt="">
+          <div class="product-container">
+            <p class="product-price">${item.priceFormatted}</p>
+            <button class="product-button">Add to Shopping Bag</button>
+          </div>
+        </article>
+      `
+    }
+
+    document.querySelector(".products").innerHTML = output;
+  }
+}
